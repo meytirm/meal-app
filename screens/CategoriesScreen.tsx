dc5a8637c502f891cdd90ae66f90f5986b4b305b
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-07-20 01:23:45 +0330

    feat(screens): add CategoriesScreen component
    Introduced `CategoriesScreen` component to display a grid of categories using `FlatList`, integrating `CATEGORIES` dummy data and the `CategoryGridTile` component for enhanced screen functionality.

---
+++ screens/CategoriesScreen.tsx
@@ -0,0 +1,18 @@
+import {FlatList} from "react-native";
+import {CATEGORIES} from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+import Category from "../models/category";
+
+function renderCategoryItem(itemData:Category) {
+  return <CategoryGridTile title={itemData.title} color={itemData.color} />
+}
+
+function CategoriesScreen() {
+  return <FlatList
+    data={CATEGORIES}
+    keyExtractor={(item) => String(item.id)}
+    renderItem={(itemData) => renderCategoryItem(itemData.item)}
+  />
+}
+
+export default CategoriesScreen
