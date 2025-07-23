665e2622bb45a1e2dc358d6eecfec97d5f8c109a
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-07-24 01:21:27 +0330

    feat(screens): enable navigation from CategoriesScreen to MealsOverviewScreen
    Added navigation functionality in `CategoriesScreen`. Integrated `onPress` handlers to `CategoryGridTile` components, allowing users to navigate to `MealsOverviewScreen` on category selection. Updated `Props` interface to define navigation prop.

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -2,12 +2,21 @@ import {FlatList} from "react-native";
 import {CATEGORIES} from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 import Category from "../models/category";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {ParamListBase} from "@react-navigation/native";
 
-function renderCategoryItem(itemData:Category) {
-  return <CategoryGridTile title={itemData.title} color={itemData.color} />
-}
+function CategoriesScreen({navigation}:Props) {
+  function renderCategoryItem(itemData: Category) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview")
+    }
 
-function CategoriesScreen() {
+    return <CategoryGridTile
+      title={itemData.title}
+      color={itemData.color}
+      onPress={pressHandler}
+    />
+  }
   return <FlatList
     data={CATEGORIES}
     keyExtractor={(item) => String(item.id)}
@@ -17,3 +26,7 @@ function CategoriesScreen() {
 }
 
 export default CategoriesScreen
+
+interface Props {
+  navigation: NativeStackNavigationProp<ParamListBase, 'MealsOverview'>;
+}
