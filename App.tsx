599ce7556a1ca9c0eeb05a249bc37973536636ae
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-07-24 01:21:46 +0330

    feat(app): add MealsOverviewScreen to navigation stack
    Added `MealsOverviewScreen` to the stack navigator in `App.tsx`. Configured navigation with a new screen, enabling transitions between `MealsCategories` and `MealsOverview`.

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import CategoriesScreen from "./screens/CategoriesScreen";
 import {StatusBar} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -12,8 +13,13 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
-            name="MealsCategories" component={CategoriesScreen}>
-          </Stack.Screen>
+            name="MealsCategories"
+            component={CategoriesScreen}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
