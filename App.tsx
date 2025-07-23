69dc46708b31a5866b8b0ed72d5efa0f49eb55a8
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-07-24 01:04:10 +0330

    feat(app): integrate navigation container and stack navigator
    Added `NavigationContainer` and `createNativeStackNavigator` to `App.tsx` to enable stack navigation. Updated the `StatusBar` style to `dark-content`. Set up the initial `MealsCategories` screen with `CategoriesScreen` as its main component.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import CategoriesScreen from "./screens/CategoriesScreen";
 import {StatusBar} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" />
-      <CategoriesScreen />
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MealsCategories" component={CategoriesScreen}>
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
 
   );
