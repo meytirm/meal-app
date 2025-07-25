78eec483989b1d0b8871d03ef7e4873df42421b6
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-07-25 18:51:50 +0330

    feat(screens): set dynamic header title in MealsOverviewScreen
    Enhanced `MealsOverviewScreen` to dynamically set the header title based on the selected category. Utilized `useLayoutEffect` with `navigation.setOptions` for runtime updates.

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,13 +1,23 @@
 import {FlatList, StyleSheet, View} from "react-native";
-import {MEALS} from "../data/dummy-data";
+import {CATEGORIES, MEALS} from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {useLayoutEffect} from "react";
 
-function MealsOverviewScreen({route}:MealsOverviewProps) {
+function MealsOverviewScreen({route, navigation}: MealsOverviewProps) {
   const catId = route.params.categoryId
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) !== -1;
   })
+
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES
+      .find((category) => category.id === catId)?.title
+    navigation.setOptions({
+      title: categoryTitle
+    })
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -36,4 +46,4 @@ const styles = StyleSheet.create({
   }
 })
 
-type MealsOverviewProps = NativeStackScreenProps<{ MealsOverview: {categoryId: string} }, 'MealsOverview'>;
+type MealsOverviewProps = NativeStackScreenProps<{ MealsOverview: { categoryId: string } }, 'MealsOverview'>;
