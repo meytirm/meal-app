a2521689d11caa919512c10cfe1a39d26d1ef581
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-07-25 13:00:35 +0330

    feat(app): update navigation styling and configure screen options
    Enhanced app navigation UI by updating `StatusBar` style to light content, setting header and content background colors, and defining header text color. Added default and specific screen options for improved user interface consistency.

--- App.tsx
+++ App.tsx
@@ -3,18 +3,32 @@ import {StatusBar} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import {RootStackParamList} from "./types";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="light-content" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={
+          {
+            headerStyle: {
+              backgroundColor: '#6e1414'
+            },
+            headerTintColor: 'white',
+            contentStyle: {
+              backgroundColor: '#452619'
+            }
+          }
+        }>
           <Stack.Screen
             name="MealsCategories"
             component={CategoriesScreen}
+            options={{
+              title: 'All Category',
+            }}
           />
           <Stack.Screen
             name="MealsOverview"
