0e17fbdeb3dcc2a827c71951ec272a3974a012c9
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-01 18:26:20 +0330

    refactor(screens): simplify MealsOverviewScreen with MealsList component
    Replaced the inline `FlatList` implementation in `MealsOverviewScreen` with the reusable `MealsList` component. Removed unused `styles` and redundant imports to streamline the screen logic and improve maintainability.

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,15 +1,14 @@
-import {FlatList, StyleSheet, View} from "react-native";
 import {CATEGORIES, MEALS} from "../data/dummy-data";
-import MealItem from "../components/MealItem";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {useLayoutEffect} from "react";
+import MealsList from "../components/MealsList/MealsList";
 
 function MealsOverviewScreen({route, navigation}: MealsOverviewProps) {
   const catId = route.params.categoryId
+
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) !== -1;
   })
-
 
   useLayoutEffect(() => {
     const categoryTitle = CATEGORIES
@@ -19,32 +18,10 @@ function MealsOverviewScreen({route, navigation}: MealsOverviewProps) {
     })
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={(itemData) =>
-          <MealItem
-            id={itemData.item.id}
-            title={itemData.item.title}
-            imageUrl={itemData.item.imageUrl}
-            duration={itemData.item.duration}
-            complexity={itemData.item.complexity}
-            affordability={itemData.item.affordability}
-          />}
-      />
-    </View>
-  )
+  return <MealsList items={displayedMeals} />
+
 }
 
 export default MealsOverviewScreen
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16
-  }
-})
 
 type MealsOverviewProps = NativeStackScreenProps<{ MealsOverview: { categoryId: string } }, 'MealsOverview'>;
