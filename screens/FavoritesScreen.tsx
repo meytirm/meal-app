29c336e17eaed1a5375b7241a43fcca5328264ce
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-01 20:35:41 +0330

    refactor(screens): migrate FavoritesScreen to use Redux for favorites
    Replaced `FavoritesContext` with Redux state management in `FavoritesScreen`. Updated logic to use `useSelector` with strong typing (`TypedUseSelectorHook`) for accessing the `favoriteMeals` state. Removed context API-related code to align with the Redux architecture.

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,13 +1,16 @@
 import {StyleSheet, Text, View} from "react-native";
 import MealsList from "../components/MealsList/MealsList";
-import {useContext} from "react";
-import {FavoritesContext} from "../store/context/favorites-context";
+// import {useContext} from "react";
+// import {FavoritesContext} from "../store/context/favorites-context";
 import {MEALS} from "../data/dummy-data";
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {RootState} from "../store/redux/store";
 
 function FavoritesScreen() {
-  const {ids} = useContext(FavoritesContext)
-
-  const favoriteMeals = MEALS.filter((meal) => ids.includes(meal.id))
+  // const {ids} = useContext(FavoritesContext)
+  const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+  const favoriteMealIds = useTypedSelector((state) => state.favoriteMeals.ids)
+  const favoriteMeals = MEALS.filter((meal) => favoriteMealIds.includes(meal.id))
 
   if (!favoriteMeals || favoriteMeals.length === 0) {
     return <View style={styles.rootContainer}>
