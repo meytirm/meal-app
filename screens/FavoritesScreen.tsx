efa49809eb9789eace268dc3e41c41bb79cf5236
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-01 18:27:46 +0330

    feat(screens): enhance FavoritesScreen with dynamic favorites display
    Replaced static text with dynamic rendering of favorite meals using `FavoritesContext`. Integrated `MealsList` component for displaying favorite meals. Added fallback UI for empty favorites, along with styling for improved visual presentation.

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,7 +1,34 @@
-import {Text} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import {useContext} from "react";
+import {FavoritesContext} from "../store/context/favorites-context";
+import {MEALS} from "../data/dummy-data";
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>
+  const {ids} = useContext(FavoritesContext)
+
+  const favoriteMeals = MEALS.filter((meal) => ids.includes(meal.id))
+
+  if (!favoriteMeals || favoriteMeals.length === 0) {
+    return <View style={styles.rootContainer}>
+      <Text style={styles.text}>No favorite meals found. Start adding some!</Text>
+    </View>
+  }
+
+  return <MealsList items={favoriteMeals}/>
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 16
+  }
+})
